7d654c4c7232bcd8895d12b1ab975b1003018fa4
Lucas Guiss <lucas.gusmao@roit.com.br>
2021-03-09 18:23:07 -0300

    fix: add @ts-ignore on unread variable

--- src/FancyLogger.ts
+++ src/FancyLogger.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs'
 export class FancyLogger {
 
     private readonly scope: string
+    //@ts-ignore
     private readonly createFile?: boolean
     private filePath: string
 
